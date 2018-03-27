0483833c2c54312fcc0d6077fa94179e04a8c1a6
1989WBJzmp <1989wangbojing@163.com>
2018-03-28 02:12:28 +0800

    1. add MIT license

--- include/nty_queue.h
+++ include/nty_queue.h
@@ -1,15 +1,28 @@
 /*
- *  Author : WangBoJing , email : 1989wangbojing@gmail.com
- * 
- *  Copyright Statement:
- *  --------------------
- *  This software is protected by Copyright and the information contained
- *  herein is confidential. The software may not be copied and the information
- *  contained herein may not be used or disclosed except with the written
- *  permission of Author. (C) 2017
- * 
+ * MIT License
  *
+ * Copyright (c) [2018] [WangBoJing]
 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ *
 ****       *****                                  ************
   ***        *                                    **   **    *
   ***        *         *                          *    **    **
@@ -39,6 +52,7 @@
 
  *
  */
+
 
 
 #ifndef __NTY_QUEUE_H__
